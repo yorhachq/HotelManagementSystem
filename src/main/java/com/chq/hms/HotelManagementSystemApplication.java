e222f8798ca3a78c64b36cdad9500ea6eedcd880
yorhachq <yorhachq@gmail.com>
2024-03-20 09:59:58 +0800

    feat: Initial project setup

---
+++ src/main/java/com/chq/hms/HotelManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.chq.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSystemApplication.class, args);
+	}
+
+}
