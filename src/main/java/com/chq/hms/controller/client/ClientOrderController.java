317537197f1b9506b035af80691e271bd067a568
yorhachq <yorhachq@gmail.com>
2024-05-01 15:15:32 +0800

    feat: Implemented frontend user interface for "My Orders" related business logic

--- src/main/java/com/chq/hms/controller/client/ClientOrderController.java
+++ src/main/java/com/chq/hms/controller/client/ClientOrderController.java
@@ -1,16 +1,19 @@
 package com.chq.hms.controller.client;
 
+import cn.hutool.core.convert.Convert;
 import com.chq.hms.anno.OperationLog;
 import com.chq.hms.domain.HotelOrder;
 import com.chq.hms.domain.Result;
-import com.chq.hms.domain.vo.CheckinVO;
 import com.chq.hms.domain.vo.HotelOrderVO;
 import com.chq.hms.domain.vo.PageBean;
 import com.chq.hms.service.HotelOrderService;
+import com.chq.hms.service.SysUserService;
+import com.chq.hms.util.ThreadLocalUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 酒店订单
@@ -21,6 +24,8 @@ public class ClientOrderController {
 
     @Autowired
     private HotelOrderService hotelOrderService;
+    @Autowired
+    private SysUserService sysUserService;
 
     /**
      * 预订酒店房间(必须提供入住和退房日期)
@@ -32,32 +37,6 @@ public class ClientOrderController {
     @PostMapping("/reserve")
     public Result<Void> reserveHotelRoom(@RequestBody HotelOrder hotelOrder) {
         hotelOrderService.reserveHotelRoom(hotelOrder);
-        return Result.success();
-    }
-
-    /**
-     * 办理入住(必须提供入住和退房日期)
-     *
-     * @param hotelOrder 酒店订单信息
-     * @return 操作结果
-     */
-    @OperationLog("业务处理：办理入住")
-    @PostMapping("/checkin")
-    public Result<Void> checkinHotelRoom(@RequestBody HotelOrder hotelOrder) {
-        hotelOrderService.checkinHotelRoom(hotelOrder);
-        return Result.success();
-    }
-
-    /**
-     * 办理退房
-     *
-     * @param orderId 订单ID
-     * @return 操作结果
-     */
-    @OperationLog("业务处理：办理退房")
-    @PutMapping("/checkout/{orderId}")
-    public Result<Void> checkoutHotelRoom(@PathVariable Integer orderId) {
-        hotelOrderService.checkoutHotelRoom(orderId);
         return Result.success();
     }
 
@@ -74,39 +53,6 @@ public class ClientOrderController {
         return Result.success();
     }
 
-//    /**
-//     * 查询酒店订单列表
-//     *
-//     * @param userId       用户ID(可选)
-//     * @param roomId       房间ID(可选)
-//     * @param orderNum     订单号(可选)
-//     * @param status       订单状态(可选)
-//     * @param reserveDate  预订日期(可选)
-//     * @param checkoutDate 退房日期(可选)
-//     * @param pageNum      页码(可选,默认为1)
-//     * @param pageSize     每页条数(可选,默认为10)
-//     * @param orderBy      排序字段(可选,默认为create_time)
-//     * @param orderType    排序方式(可选,默认为desc)
-//     * @return 酒店订单列表
-//     */
-//    @GetMapping
-//    public Result<PageBean<HotelOrder>> getHotelOrders(
-//            @RequestParam(required = false) Integer userId,
-//            @RequestParam(required = false) Integer roomId,
-//            @RequestParam(required = false) Integer roomNumber,
-//            @RequestParam(required = false) String orderNum,
-//            @RequestParam(required = false) String status,
-//            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date reserveDate,
-//            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date checkinDate,
-//            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date checkoutDate,
-//            @RequestParam(defaultValue = "1") Integer pageNum,
-//            @RequestParam(defaultValue = "10") Integer pageSize,
-//            @RequestParam(defaultValue = "create_time") String orderBy,
-//            @RequestParam(defaultValue = "desc") String orderType) {
-//        PageBean<HotelOrder> pageInfo = hotelOrderService.getHotelOrders(userId, roomId, roomNumber, orderNum, status, reserveDate, checkinDate, checkoutDate, pageNum, pageSize, orderBy, orderType);
-//        return Result.success(pageInfo);
-//    }
-
     /**
      * 获取酒店订单列表(VO)
      *
@@ -116,8 +62,6 @@ public class ClientOrderController {
      * @param checkoutDate    退房日期
      * @param status          订单状态
      * @param orderNum        订单号
-     * @param username        用户名
-     * @param phone           用户手机号
      * @param roomNumber      房间号
      * @param pageNum         当前页码
      * @param pageSize        每页记录数
@@ -131,44 +75,17 @@ public class ClientOrderController {
                                                          @RequestParam(required = false) String checkoutDate,
                                                          @RequestParam(required = false) String status,
                                                          @RequestParam(required = false) String orderNum,
-                                                         @RequestParam(required = false) String username,
-                                                         @RequestParam(required = false) String phone,
                                                          @RequestParam(required = false) Integer roomNumber,
                                                          @RequestParam(defaultValue = "1") Integer pageNum,
                                                          @RequestParam(defaultValue = "10") Integer pageSize,
                                                          @RequestParam(defaultValue = "create_time") String orderBy,
                                                          @RequestParam(defaultValue = "desc") String orderType) {
+        // 固定为会员自身的订单
+        Map<String, Object> data = ThreadLocalUtil.get();
+        Integer userId = Convert.toInt(data.get("id"));
+        String currtentUser = sysUserService.findByUserId(userId).getUsername();
         PageBean<HotelOrderVO> pageInfo = hotelOrderService.getHotelOrders(createTimeRange, reserveDate, checkinDate,
-                checkoutDate, status, orderNum, username, phone, roomNumber, pageNum, pageSize, orderBy, orderType);
-        return Result.success(pageInfo);
-    }
-
-    /**
-     * 获取入住管理列表
-     *
-     * @param orderNum     订单号
-     * @param roomNumber   房间号
-     * @param checkinDate  入住日期
-     * @param checkoutDate 退房日期
-     * @param pageNum      当前页码
-     * @param pageSize     每页记录数
-     * @param status       订单状态
-     * @param orderBy      排序字段(可选,默认为checkin_date)
-     * @param orderType    排序方式(可选,默认为desc)
-     * @return 入住管理列表分页数据
-     */
-    @OperationLog("数据获取：入住信息列表")
-    @GetMapping("/checkinList")
-    public Result<PageBean<CheckinVO>> getCheckinList(@RequestParam(required = false) String orderNum,
-                                                      @RequestParam(required = false) Integer roomNumber,
-                                                      @RequestParam(required = false) String checkinDate,
-                                                      @RequestParam(required = false) String checkoutDate,
-                                                      @RequestParam(defaultValue = "1") Integer pageNum,
-                                                      @RequestParam(defaultValue = "7") Integer pageSize,
-                                                      @RequestParam(defaultValue = "入住中") String status,
-                                                      @RequestParam(defaultValue = "checkin_date") String orderBy,
-                                                      @RequestParam(defaultValue = "desc") String orderType) {
-        PageBean<CheckinVO> pageInfo = hotelOrderService.getCheckinList(orderNum, roomNumber, checkinDate, checkoutDate, pageNum, pageSize, status, orderBy, orderType);
+                checkoutDate, status, orderNum, currtentUser, null, roomNumber, pageNum, pageSize, orderBy, orderType);
         return Result.success(pageInfo);
     }
 }
